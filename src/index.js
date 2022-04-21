c51ab90ec61a49015a03211fe575e06d8cfeda66
miogy <jjangrl87@gmail.com>
2022-04-21 20:26:33 +0900

    addApointment

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // component
+import AddApointment from './components/AddApointment'
 
 // source
+import { BiArchive } from "react-icons/bi";
+// react-icons 사이트로 들어가서 가져오기
 import './index.css'
 
 function App(){
   return (
     <article>
-      <h3>welcome</h3>
-      <div>
+      <h3>
+        <BiArchive style={{color:'#d47776'}}/> 예약시스템
+      </h3>
+      <AddApointment />
+      <div id="list">
         <ul>
-          <li></li>
+          <li>반복문</li>
         </ul>
       </div>
     </article>
